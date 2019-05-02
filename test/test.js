04bfc95c82cc4afc7dbc36a2645381f021f6fe3d
ABM97 <viragesud97@gmail.com>
2019-05-02 23:26:59 +0200

    Testing Travis

---
+++ test/test.js
@@ -0,0 +1,14 @@
+const request = require('request');
+const expect = require('chai').expect;
+const app = require('../index')
+
+
+describe('Server response', function () {
+    it('should return 200', function (done) {
+        request.get('http://localhost:8080/', function (err, res, body) {
+            if (err) throw err;
+            expect(res.statusCode).to.equal(200);
+            done();
+        })
+    })
+})
