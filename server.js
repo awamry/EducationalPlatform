11ddb17577fc70eeadab2416ccef52e526b988f4
Awamry <omarelawamry@gmail.com>
2019-05-03 13:44:57 +0200

    Add editorconfig

---
+++ server.js
@@ -0,0 +1,38 @@
+const db = require('./database/config/database');
+const Games = require('./database/models/Games');
+const Questions = require('./database/models/questions');
+const connectSequelize = async () => {
+    try {
+        await db.authenticate();
+        console.log("Successfully connected to the database");
+        console.log("Please wait until the tables are successfully created");
+
+
+    } catch (e) {
+        console.log("error");
+
+    }
+}
+
+connectSequelize();
+
+const checkDB = async () => {
+    await db.sync({ force: true });
+    await Games.create({
+        name: 'Name',
+        type: 'True or false'
+    });
+    await Questions.create({
+        question: 'My name is omar',
+        answer: 'true',
+        gameId: 1
+    });
+
+}
+
+checkDB();
+
+
+
+// NOTE: BEFORE RUNNING THIS FILE YOU SHOULD CREATE A DATABASE CALLED "games" in postgres.
+//then when you run this file all of the tables will be created automatically
--- test/test.js
+++ test/test.js
@@ -1,14 +1,18 @@
-const request = require('request');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 const expect = require('chai').expect;
-const app = require('../index')
+const app = require('../lib/index');
 
-
+chai.use(chaiHttp);
+var url = 'http://localhost:3000';
+var requester = chai.request.agent(url);
 describe('Server response', function () {
     it('should return 200', function (done) {
-        request.get('http://localhost:8080/', function (err, res, body) {
-            if (err) throw err;
-            expect(res.statusCode).to.equal(200);
-            done();
-        })
-    })
-})
+        requester
+            .get('/')
+            .end(function (err, res) {
+                expect(res).to.have.status(200);
+                done(); // <= Call done to signal callback end
+            });
+    });
+});
