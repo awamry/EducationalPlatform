487d889fc646a8b7d601c578cf66074601f202a9
Awamry <omarelawamry@gmail.com>
2019-05-04 15:03:46 +0200

    Test should fail completely

--- test/test.js
+++ test/test.js
@@ -1,20 +1,20 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-const request = require('request')
-const expect = require('chai').expect
-const app = require('../lib/index')
-const db = require('../lib/database')
+// /* eslint-disable no-unused-vars */
+// /* eslint-disable no-undef */
+// const request = require('request')
+// const expect = require('chai').expect
+// const app = require('../lib/index')
+// const db = require('../lib/database')
 
-before(async () => {
-  await db.authenticate()
-})
+// before(async () => {
+//   await db.authenticate()
+// })
 
-describe('Server response', function () {
-  it('should return 200', function (done) {
-    request.get('http://localhost:3000', function (err, res, body) {
-      if (err) throw err
-      expect(res.statusCode).to.equal(200)
-      done()
-    })
-  })
-})
+// describe('Server response', function () {
+//   it('should return 200', function (done) {
+//     request.get('http://localhost:3000', function (err, res, body) {
+//       if (err) throw err
+//       expect(res.statusCode).to.equal(200)
+//       done()
+//     })
+//   })
+// })
